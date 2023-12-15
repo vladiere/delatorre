8fc6ae1177299748eec5ae3430548919e9bdb37e
vladiere <ruthakaza@gmail.com>
2023-12-16 01:05:25 +0800

    button

--- src/components/AboutComponent.vue
+++ src/components/AboutComponent.vue
@@ -10,10 +10,10 @@
 </style>
 
 <template>
-  <div :class="Platform.is.mobile ? 'column justify-center bg-dark text-grey-2' : 'window-height column justify-center bg-dark text-grey-2'">
-    <div :class="Platform.is.mobile ? 'column reverse q-gutter-y-md items-center q-pa-md' : 'row q-px-xl q-gutter-x-lg items-center'">
+  <div :class="Platform.is.mobile ? 'window-height column justify-center bg-dark text-grey-2' : 'window-height column justify-center bg-dark text-grey-2'">
+    <div :class="Platform.is.mobile ? 'column reverse items-center q-pa-md' : 'row q-px-xl q-gutter-x-lg items-center'">
       <div class="col column q-gutter-y-md">
-        <span :class="Platform.is.mobile ? 'text-center text-h6 text-weight-light' : 'text-h6 text-weight-light'">
+        <span :class="Platform.is.mobile ? 'text-center text-subtitle1 text-weight-light' : 'text-h6 text-weight-light'">
           Greetings! I'm John Patrick Delatorre, a devoted software engineer specializing in web, mobile, and desktop applications. My expertise centers around JavaScript, and I'm dedicated to making the web more accessible. I will earned my bachelor's degree in Information Technology in Cordova Public College. Eager to explore job opportunities aligned with my passion and skills.
         </span>
         <q-btn outline color="accent" label="Skills" no-caps class="self-start" size="xl" @click="scrollToComponent"/>
